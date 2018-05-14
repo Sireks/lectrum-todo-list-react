657a5d00e269d0cbfb9f3debb76e68bfb9b6cbbb
Sergiy Tkachenko <sireks20@gmail.com>
2018-05-14 22:54:56 +0300

    init(jsx, components, postCss): add basic html markup

---
+++ source/containers/App/index.js
@@ -0,0 +1,14 @@
+// Core
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+import Todo from 'components/Todo'
+
+@hot(module)
+export default class App extends Component {
+    render () {
+        return (
+            <Todo />
+        );
+    }
+}
